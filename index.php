2829cfbb7157eff54981ad566dfa46def5408e69
Devian <forza9882@gmail.com>
2017-04-07 16:06:54 +0200

    site landing page update

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="UWiW-71Grk3ni4Dul52yMz5J3X_LUHazEpjaFROJv0k" />
+        <link rel="icon" href="favicon.ico" type="image/x-icon" sizes="16x16">
+        <title>DevianK</title>
+        <script src="https://use.fontawesome.com/d6aded9c3f.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link type="text/css" rel="stylesheet" href="myStyle.css">
+
+    </head>
+    <body>
+    	<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>                        
+		      </button>
+		      <a class="navbar-brand" href="">DevianK</a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="myNavbar">
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="http://devian.co.za/wp/">My Blog</a></li>
+		        <li><a href="http://devian.co.za/wp/shop/">SHOP</a></li>
+		      </ul>
+		    </div>
+		  </div>
+	</nav>   
+ 
+        <div class="container-fluid">
+            <div id="profilePic">                
+                    <img src="images/DK2.jpg" class="img-responsive img-circle center-block" alt="profile_image">                
+            </div>
+            <a href="http://devian.co.za/wp/"><i class="fa fa-chevron-circle-left fa-4x" title="Enter my world!" aria-hidden="true"></i></a>
+            <a href="http://devian.co.za/wp/shop/"><i class="fa fa-chevron-circle-right fa-4x" title="Enter my world!"aria-hidden="true"></i></a>
+
+            <h1 class="text-center">Devian Kapp</h1>
+            <h3>Software Developer<div class="cursor"></div></h3>
+            
+            
+        </div>
+    </body>
+</html>
